b38cb787b5db4152f721c81008e3d6d56969d329
suhsein <mini9075@gmail.com>
2023-02-25 19:14:19 +0900

    fix : index.js 수정 index.html 버튼 클릭 시 페이지 로드 되지 않는 문제 발생

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
-$translate = document.querySelector("#translate");
-$textract = document.querySelector("#textract");
-$translateNtextract = document.querySelector("#translateNtextract");
+window.onload = function() {
+  $translate = document.querySelector("#translate");
+  $textract = document.querySelector("#textract");
+  $translateNtextract = document.querySelector("#translateNtextract");
 
-$translate.addEventListener("click", () => {
-  window.open("./translate.html", (target = "_blank"));
-});
-$textract.addEventListener("click", () => {
-  window.open("./textract.html", (target = "_blank"));
-});
-$translateNtextract.addEventListener("click", () => {
-  window.open("./translateNtextract.html", (target = "_blank"));
-});
+  $translate.addEventListener("click", () => {
+    window.open("./translate.html", (target = "_blank"));
+  });
+  $textract.addEventListener("click", () => {
+    window.open("./textract.html", (target = "_blank"));
+  });
+  $translateNtextract.addEventListener("click", () => {
+    window.open("./translateNtextract.html", (target = "_blank"));
+  });
 
-new ClipboardJS(".btn");
-function msg() {
-  alert("복사되었습니다.");
+  new ClipboardJS(".btn");
+  function msg() {
+    alert("복사되었습니다.");
+  }
+  $email.addEventListener("click", msg);
 }
-$email.addEventListener("click", msg);
